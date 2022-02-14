393ac0998f26631328537705c1f15b9e4c21954c
Jordan_Snow <jordantreysnow@gmail.com>
2022-02-13 20:55:54 -0700

    Integrated the update method to file

--- Main.java
+++ Main.java
@@ -16,6 +16,7 @@ import java.util.*;
 public class Main {
     /** enter path to file, set filePath equal to it **/
     String filePath;
+
     /** method to convert file into ArrayList **/
     public void csvToArrayList(String file) {
 
@@ -26,45 +27,47 @@ public class Main {
 
     }
 
-    /** search helper method. searches array list, returns index of array w/ matching product ID**/
+    /**
+     * search helper method. searches array list, returns index of array w/ matching
+     * product ID
+     **/
     public int search(int id) {
 
     }
-     /** DEFAULT CONSTRUCTOR to access and run methods **/
-     public Main(){
 
-     }
+    /** DEFAULT CONSTRUCTOR to access and run methods **/
+    public Main() {
+
+    }
 
     /** MAIN LOOP **/
-    public static void main (String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
 
-        //Main manageInv = new Main(); 
+        // Main manageInv = new Main();
         Scanner userInput = new Scanner(System.in);
         String fileName = "inventory_team4.csv"; // variable for hardcoded file name
-
 
         boolean run = true;
         while (run) {
 
             int selection;
-            
+
             System.out.println("Choose 1 to Create");
             System.out.println("Choose 2 to Read");
             System.out.println("Choose 3 to Update");
             System.out.println("Choose 4 to Delete");
             System.out.println("Choose 5 to EXIT");
             selection = userInput.nextInt();
-            
 
             switch (selection) {
                 case 1:
 
-                    /** prompt user for product id, index 0**/
+                    /** prompt user for product id, index 0 **/
                     System.out.println("You have chosen to create a new product.\n");
-                    
+
                     System.out.println("Enter new product ID:");
                     String prodID = userInput.nextLine();
-                    //prodID = userInput.nextLine();
+                    prodID = userInput.nextLine();
 
                     /** prompt user for quantity, index 1 **/
                     System.out.println("Enter product quantity:");
@@ -83,7 +86,7 @@ public class Main {
                     String supplierID = userInput.nextLine();
                     supplierID = userInput.nextLine();
 
-                    /** METHOD CALL   ADJUST AS NEEDED **/
+                    /** METHOD CALL ADJUST AS NEEDED **/
                     Main.addRecord(prodID, quant, wholesaleCost, salePrice, supplierID, fileName);
 
                     break;
@@ -96,11 +99,11 @@ public class Main {
                     int readChoice = userInput.nextInt();
                     if (readChoice == 1) {
                         Main.readAll();
-                    } else if (readChoice == 2){
+                    } else if (readChoice == 2) {
                         System.out.println("Enter product ID of item:");
                         String readProdID = userInput.nextLine();
                         /** if product ID exists, call readOne method. if not, throw exception. **/
-                    } else{
+                    } else {
                         System.out.println("Oops! You have entered an invalid choice!");
                     }
 
@@ -110,28 +113,33 @@ public class Main {
                     /** ADJUST AS NEEDED TO WORK WITH UPDATE METHOD **/
                     System.out.println("You have chosen to update an item.");
 
-                    /** input product id of product to update**/
+                    /** input product id of product to update **/
                     System.out.println("Enter product ID to update:");
-                    int updateID = userInput.nextInt();
+                    String updateID = userInput.nextLine();
+                    updateID = userInput.nextLine();
+                    String removalID = updateID;
+
                     System.out.println("Enter new values. If a value must stay the same, enter nothing.");
 
                     System.out.println("Enter new quantity:");
                     int updateQuant = userInput.nextInt();
+                    // updateQuant = userInput.nextInt();
 
-                    
                     System.out.println("Enter new wholesale cost:");
                     float updateWholesale = userInput.nextFloat();
+                    // updateWholesale = userInput.nextFloat();
 
-                    
                     System.out.println("Enter new sale price:");
                     float updatePrice = userInput.nextFloat();
+                    // updatePrice = userInput.nextFloat();
 
-                    
                     System.out.println("Enter new supplier ID:");
-                    int updateSupplierID = userInput.nextInt();
+                    String updateSupplierID = userInput.nextLine();
+                    updateSupplierID = userInput.nextLine();
 
                     /** UPDATE METHOD CALL **/
-                    update(updateID, updateQuant, updateWholesale, updatePrice, updateSupplierID);
+                    Main.update(updateID, updateQuant, updateWholesale, updatePrice, updateSupplierID, fileName,
+                            removalID);
 
                     break;
 
@@ -144,7 +152,7 @@ public class Main {
 
                     /** METHOD CALL **/
                     Main.deleteRecord(deleteID);
-                    
+
                     break;
 
                 case 5:
@@ -158,40 +166,43 @@ public class Main {
     /**
      * input '1'
      * CREATE METHOD called from case 1 of switch case
-     * takes in 5 parameters, turns into array, appends array to end of arraylist (csv file)
-     * **/
-    //Method that adds records to the inventory file
-  //Implemented by Kyle Zimmerman 2.12.22
-  public static void addRecord(String productIdIn, int quantityIn, float wholesaleCostIn, float salePriceIn, String supplierIdIn, String file){
+     * takes in 5 parameters, turns into array, appends array to end of arraylist
+     * (csv file)
+     **/
+    // Method that adds records to the inventory file
+    // Implemented by Kyle Zimmerman 2.12.22
+    public static void addRecord(String productIdIn, int quantityIn, float wholesaleCostIn, float salePriceIn,
+            String supplierIdIn, String file) {
 
-    
+        BufferedWriter buffWrite = null; // Setting up the buffered writer variable and setting initially to null
+        // String product_id, quantity, wholesale_cost, sale_price, supplier_id;
+        // //Setting up all the sting variables that I will be sett to user inputs
+        Float wholesaleCost = wholesaleCostIn;
+        Float salePrice = salePriceIn;
+        String productId = productIdIn;
+        String supplierId = supplierIdIn;
+        String fileName = file;
+        int quantity = quantityIn;
 
-
-    BufferedWriter buffWrite = null; //Setting up the buffered writer variable and setting initially to null
-    //String product_id, quantity, wholesale_cost, sale_price, supplier_id; //Setting up all the sting variables that I will be sett to user inputs
-    Float wholesaleCost = wholesaleCostIn;
-    Float salePrice = salePriceIn;
-    String productId = productIdIn;
-    String supplierId = supplierIdIn;
-    String fileName = file;
-    int quantity = quantityIn;
-    
-
-    
-    // Writing to the inventory file
-    // Try catches IOExecption
-    try { 
-      buffWrite = new BufferedWriter(new FileWriter(fileName,true)); //Setting our buffered writer to the correct file
-      buffWrite.write(productId+","+quantity+","+wholesaleCost+","+salePrice+","+supplierId); //Writing to the file
-      buffWrite.flush();
-      buffWrite.newLine();
-      buffWrite.close();
-      System.out.println("Added "+productId+","+quantity+","+wholesaleCost+","+salePrice+","+supplierId+" to the inventory!\n");
-    } catch (IOException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
+        // Writing to the inventory file
+        // Try catches IOExecption
+        try {
+            buffWrite = new BufferedWriter(new FileWriter(fileName, true)); // Setting our buffered writer to the
+                                                                            // correct file
+            buffWrite.write(productId + "," + quantity + "," + wholesaleCost + "," + salePrice + "," + supplierId); // Writing
+                                                                                                                    // to
+                                                                                                                    // the
+                                                                                                                    // file
+            buffWrite.flush();
+            buffWrite.newLine();
+            buffWrite.close();
+            System.out.println("Added " + productId + "," + quantity + "," + wholesaleCost + "," + salePrice + ","
+                    + supplierId + " to the inventory!\n");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
-  }
 
     /** input '2' **/
     /** READ METHOD **/
@@ -203,29 +214,26 @@ public class Main {
         BufferedReader reader = null; // new reader to be able to read the .csv file
         String line = ""; // variable to read each line within the file
 
-        try{
-            reader = new BufferedReader(new FileReader(file)); //instatiates the reader to read file 
-            
-            // Continuously read the next line until there is no more data to be read 
-            while((line = reader.readLine())!= null){
-                String[] row = line.split(","); //split each line at the commas
+        try {
+            reader = new BufferedReader(new FileReader(file)); // instatiates the reader to read file
 
-                // For each row of data, print the comma separated values 
-                for(String index : row){
+            // Continuously read the next line until there is no more data to be read
+            while ((line = reader.readLine()) != null) {
+                String[] row = line.split(","); // split each line at the commas
+
+                // For each row of data, print the comma separated values
+                for (String index : row) {
                     System.out.printf("%-15s", index);
                 }
                 System.out.println();
 
             }
-        }
-        catch(IOException err){
+        } catch (IOException err) {
             err.printStackTrace();
-        }
-        finally{
-            try{
-            reader.close();
-            }
-            catch(IOException err){
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException err) {
                 err.printStackTrace();
             }
         }
@@ -233,16 +241,33 @@ public class Main {
 
     /** if one, prompt for product id and print product array to console **/
     public static int[] readOne(int productID) {
-        /** use search() method if needed**/
+        /** use search() method if needed **/
     }
 
     /** input '3' **/
     /** UPDATE METHOD **/
-    /** note: if nothing is entered for a prompt, the current value will stay the same. **/
-    public static void update(int prodID, int newQuant, float newWholesale, float newPrice, int newSupplierID) {
+    /**
+     * note: if nothing is entered for a prompt, the current value will stay the
+     * same.
+     **/
+
+    public static void update(String updateID, int updateQuant, float updateWholesale, float updatePrice,
+            String updateSupplierID, String fileName, String removalID) throws IOException {
+
+        String deleteProduct = removalID;
+        String prodID = updateID;
+        int quanity = updateQuant;
+        float whole_sale = updateWholesale;
+        float sale_price = updatePrice;
+        String supplier_ID = updateSupplierID;
+        String updateFile = fileName;
+
+        deleteRecord(deleteProduct);
+        addRecord(prodID, quanity, whole_sale, sale_price, supplier_ID, updateFile);
+
+        System.out.println("Your file has been updated.");
 
     }
-
 
     /** input '4' **/
     /** DELETE METHOD **/
@@ -250,34 +275,34 @@ public class Main {
 
         String product_ID = deleteID;
         String row;
-        String data[];  
-        
-        File tempFile = new File("inventory_temp_team4.csv");   //Temp file to add rows were are not deleting.
-        File file = new File("inventory_team4.csv");            // original file.
+        String data[];
+
+        File tempFile = new File("inventory_temp_team4.csv"); // Temp file to add rows were are not deleting.
+        File file = new File("inventory_team4.csv"); // original file.
         FileReader reader = new FileReader(file);
-        BufferedReader read = new BufferedReader(reader);      
+        BufferedReader read = new BufferedReader(reader);
         FileWriter writer = new FileWriter(tempFile);
         BufferedWriter write = new BufferedWriter(writer);
         PrintWriter print = new PrintWriter(write);
 
-        //Loop through each line to look for productID
-        while((row = read.readLine())!= null){
+        // Loop through each line to look for productID
+        while ((row = read.readLine()) != null) {
             data = row.split(",");
-        // If productID does not match data then print to temp file.     
-          if(!(data[0].equals(product_ID))){        
-            print.println(row);            
-          }    
-         } 
-         System.out.println("Product deleted succesfully"); 
-         //close utilities
-          print.flush();
-          print.close();
-          writer.close();
-          write.close();
-          read.close();
-          reader.close();
-          file.delete();
-          tempFile.renameTo(file);  //Rename file.
-          
-        }  
+            // If productID does not match data then print to temp file.
+            if (!(data[0].equals(product_ID))) {
+                print.println(row);
+            }
+        }
+        System.out.println("Product deleted succesfully");
+        // close utilities
+        print.flush();
+        print.close();
+        writer.close();
+        write.close();
+        read.close();
+        reader.close();
+        file.delete();
+        tempFile.renameTo(file); // Rename file.
+
+    }
 }
