7bab59d2d297aa31f6bbb35c5b29acc7b4d9a60d
Chillyssa <38333607+chillyssa@users.noreply.github.com>
2022-02-13 23:08:32 -0700

    Formatted the print statements to display a more organized System menu for the user

--- Main.java
+++ Main.java
@@ -16,7 +16,7 @@ import java.util.*;
 public class Main {
     /** enter path to file, set filePath equal to it **/
 
-    String filePath;
+    String filePath="inventory_team4.csv";
     /** method to convert file into ArrayList **/
     public void csvToArrayList(String file) {
 
@@ -53,18 +53,19 @@ public class Main {
         while (run) {
 
             int selection;
-
             System.out.println("Choose 1 to Create");
             System.out.println("Choose 2 to Read");
             System.out.println("Choose 3 to Update");
             System.out.println("Choose 4 to Delete");
             System.out.println("Choose 5 to EXIT");
+            System.out.println("-------------------------------------");
             selection = userInput.nextInt();
 
             switch (selection) {
                 case 1:
 
                     /** prompt user for product id, index 0 **/
+                    System.out.println("-----------------------------------------------------------------------------------");
                     System.out.println("You have chosen to create a new product.\n");
 
                     System.out.println("Enter new product ID:");
@@ -90,10 +91,11 @@ public class Main {
 
                     /** METHOD CALL ADJUST AS NEEDED **/
                     Main.addRecord(prodID, quant, wholesaleCost, salePrice, supplierID, fileName);
-
+                    System.out.println("-----------------------------------------------------------------------------------");
                     break;
 
                 case 2:
+                    System.out.println("-----------------------------------------------------------------------------------");
                     System.out.println("You have chosen to read the file.");
                     System.out.println("What do you want to view");
                     System.out.println("1. Display All Inventory");
@@ -106,16 +108,16 @@ public class Main {
                         String readProdID = userInput.nextLine();
                         readProdID = userInput.nextLine();
                         /** if product ID exists, call readOne method. if not, throw exception. **/
-                    } else {
                         Main.readOne(readProdID);
                     } else{
                         System.out.println("Oops! You have entered an invalid choice!");
                     }
-
+                    System.out.println("-----------------------------------------------------------------------------------");
                     break;
 
                 case 3:
-                    /** ADJUST AS NEEDED TO WORK WITH UPDATE METHOD **/
+                    /** UPDATE METHOD **/
+                    System.out.println("-----------------------------------------------------------------------------------");
                     System.out.println("You have chosen to update an item.");
 
                     /** input product id of product to update **/
@@ -124,19 +126,17 @@ public class Main {
                     updateID = userInput.nextLine();
                     String removalID = updateID;
 
-                    System.out.println("Enter new values. If a value must stay the same, enter nothing.");
+                    System.out.println("Enter updated product information");
 
                     System.out.println("Enter new quantity:");
                     int updateQuant = userInput.nextInt();
-                    // updateQuant = userInput.nextInt();
 
                     System.out.println("Enter new wholesale cost:");
                     float updateWholesale = userInput.nextFloat();
-                    // updateWholesale = userInput.nextFloat();
+                    
 
                     System.out.println("Enter new sale price:");
                     float updatePrice = userInput.nextFloat();
-                    // updatePrice = userInput.nextFloat();
 
                     System.out.println("Enter new supplier ID:");
                     String updateSupplierID = userInput.nextLine();
@@ -145,23 +145,28 @@ public class Main {
                     /** UPDATE METHOD CALL **/
                     Main.update(updateID, updateQuant, updateWholesale, updatePrice, updateSupplierID, fileName,
                             removalID);
-
+                    System.out.println("-----------------------------------------------------------------------------------");
                     break;
 
                 case 4:
-
+                    /** DELETE METHOD **/
+                    System.out.println("-----------------------------------------------------------------------------------");
                     System.out.println("You have chosen to delete an item.");
                     System.out.println("Enter product ID of item to delete:");
                     String deleteID = userInput.nextLine();
                     deleteID = userInput.nextLine();
 
-                    /** METHOD CALL **/
+                    /** DELETE METHOD CALL **/
                     Main.deleteRecord(deleteID);
-
+                    System.out.println("-----------------------------------------------------------------------------------");
                     break;
 
                 case 5:
-                    System.out.println("Bye!");
+                    /** SYSTEM EXIT **/
+                    System.out.println("-----------------------------------------------------------------------------------");
+                    System.out.println("Exiting the Inventory Management System. Have a nice day!");
+                    System.out.println("-----------------------------------------------------------------------------------");
+                    userInput.close();
                     run = false;
                     break;
             }
@@ -194,17 +199,15 @@ public class Main {
         try {
             buffWrite = new BufferedWriter(new FileWriter(fileName, true)); // Setting our buffered writer to the
                                                                             // correct file
-            buffWrite.write(productId + "," + quantity + "," + wholesaleCost + "," + salePrice + "," + supplierId); // Writing
-                                                                                                                    // to
-                                                                                                                    // the
-                                                                                                                    // file
+            buffWrite.write(productId + "," + quantity + "," + wholesaleCost + "," + salePrice + "," + supplierId); // Writing to the file 
             buffWrite.flush();
             buffWrite.newLine();
             buffWrite.close();
-            System.out.println("Added " + productId + "," + quantity + "," + wholesaleCost + "," + salePrice + ","
-                    + supplierId + " to the inventory!\n");
+            System.out.println("Successfully added the product to inventory");
+            System.out.println("Product ID:\t"+ productId + "\nQuantity:\t" + quantity + "\nWholesale Cost:\t" + wholesaleCost + "\nSale Price:\t" + salePrice + "\nSupplier ID:\t" + supplierId);
+            System.out.println("-----------------------------------------------------------------------------------");
         } catch (IOException e) {
-            System.out.println("An error occurred.");
+            System.out.println("Oops! An error has occurred! Please try again.");
             e.printStackTrace();
         }
     }
@@ -212,7 +215,7 @@ public class Main {
     /** input '2' **/
     /** READ METHOD **/
     /** read all or one product? **/
-    /** if all, loop through ArrayList and print all to console **/
+    /** if all, loop through array and print all to console **/
 
     public static void readAll() {
         String file = "test.csv"; // variable to reference .csv file
@@ -245,15 +248,12 @@ public class Main {
     }
 
     /** if one, prompt for product id and print product array to console **/
-    public static int[] readOne(int productID) {
-        /** use search() method if needed **/
     public static void readOne(String readProdID) {
-         /** use search() method if needed**/
         String readOneID = readProdID;
-        String file = "test.csv";
+        String file = "inventory_team4.csv";
         BufferedReader reader = null; // new reader to be able to read the .csv file
         String line = ""; // variable to read each line within the file
-        String titleRow[] = {"product_id","quantity","wholesale_cost","sale_price","supplier_id"};
+        String titleRow[] = {"Product ID","Quantity","Wholesale Cost","Sale Price","Supplier ID"};
         try{
             reader = new BufferedReader(new FileReader(file)); //instatiates the reader to read file 
             
@@ -265,6 +265,7 @@ public class Main {
                         System.out.printf("%-15s", title);
                     }
                     System.out.println();
+                    System.out.println("-----------------------------------------------------------------------------------");
                     for(String index : row){
                         System.out.printf("%-15s", index);
                     }
@@ -306,7 +307,9 @@ public class Main {
         deleteRecord(deleteProduct);
         addRecord(prodID, quanity, whole_sale, sale_price, supplier_ID, updateFile);
 
-        System.out.println("Your file has been updated.");
+        System.out.println("Your file has been Successfully updated!");
+        System.out.println("-----------------------------------------------------------------------------------");
+
 
     }
 
@@ -334,7 +337,8 @@ public class Main {
                 print.println(row);
             }
         }
-        System.out.println("Product deleted succesfully");
+        System.out.println("Product deleted succesfully!");
+        System.out.println("-----------------------------------------------------------------------------------");
         // close utilities
         print.flush();
         print.close();
