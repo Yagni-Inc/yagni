3ea340d3b63144ee892678079360ca7e8ec150d0
Jesus Fuentes <jesusxx240@gmail.com>
2022-02-24 19:16:28 -0700

    Added ordersGUI class and added a new GUI for it

--- Main_Project/src/Views/ManageInventoryGUI.java
+++ Main_Project/src/Views/ManageInventoryGUI.java
@@ -1,22 +1,19 @@
 package Main_Project.src.Views;
-import java.awt.Color;
-import javax.swing.JButton;
+import java.awt.Font;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class ManageInventoryGUI {
-
 
         JFrame userFrame = new JFrame("Product Inventory Window");  
         JLabel userLabel = new JLabel();
 
         ManageInventoryGUI(){
 
-            userLabel.setText("This will show CRUD operations.");   // Window for future CRUD operations
-            userLabel.setBounds(325, 50, 85, 25);
+            userLabel.setText("This will eventualy show CRUD operations and Inventory.");  // Window for future CRUD operations
+            userLabel.setBounds(325, 80, 85, 25);
+            userLabel.setFont(new Font("Calibri",Font.BOLD,20));
             userLabel.setHorizontalAlignment(JLabel.CENTER);
             userLabel.setVerticalAlignment(JLabel.TOP);
 
@@ -26,8 +23,4 @@ public class ManageInventoryGUI {
             userFrame.setVisible(true);
 
         }
-
-        
-
-    
 }
