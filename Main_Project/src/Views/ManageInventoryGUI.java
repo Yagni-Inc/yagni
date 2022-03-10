dd698d8707341039ae4fafe5ac58bc40dfaff4d0
S4kRiPp4 <98367301+S4kRiPp4@users.noreply.github.com>
2022-03-09 18:49:38 -0700

    YAG-35-add-search-button-to-controls-panel

--- Main_Project/src/Views/ManageInventoryGUI.java
+++ Main_Project/src/Views/ManageInventoryGUI.java
@@ -11,42 +11,52 @@ import Main_Project.src.Controller.*;
 import Main_Project.src.Model.Create;
 import Main_Project.src.Model.ReadAll;
 
-public class ManageInventoryGUI implements ActionListener{
+public class ManageInventoryGUI implements ActionListener,FocusListener{
 
         private static JFrame inventoryFrame = new JFrame("Product Inventory Window");  
-        private static JLabel inventoryLabel = new JLabel();
-        private static JPanel headerPanel = new JPanel();              //Creates header content area
+        private static JTable productsTable = new JTable();
+        private static JScrollPane tableScroll = new JScrollPane();
+        private static DbConnection linkDB;
+        
+        
+        private static JPanel headerPanel = new JPanel();               //Creates header content are
+        private static JPanel navPanel = new JPanel();             
         private static JPanel bodyPanel = new JPanel();                //Creates body content area 
         private static JPanel footerPanel = new JPanel();              //Creates footer content area 
-        private static JLabel headerLabel = new JLabel();              //text/image label for header 
-        private static JLabel footerLabel = new JLabel();              //text label for footer
-        private static ImageIcon logoImg = new ImageIcon("Main_Project/assets/img/YagniLogoOnly-50percent.png"); //load logo image  
-
         private static JPanel tablePanel = new JPanel();               //Creates panel for table area
         private static JPanel controlsPanel = new JPanel();            //Creates panel for controls area
+       
+        private static JLabel headerLabel = new JLabel();              //text/image label for header 
+        private static JLabel footerLabel = new JLabel();              //text label for footer
+        private static JLabel controlsTitle = new JLabel("Actions");
+        private static JLabel productIDLabel = new JLabel("Product ID");
+        private static JLabel quantityLabel = new JLabel("Quantity");
+        private static JLabel wholeSaleLabel = new JLabel("Whole Sale Price");
+        private static JLabel salePriceLabel = new JLabel("Sale Price");
+        private static JLabel supplierIDLabel = new JLabel("Supplier ID");
+
+        private static ImageIcon logoImg = new ImageIcon("Main_Project/assets/img/YagniLogoOnly-50percent.png"); //load logo image  
+        private static ImageIcon searchImg = new ImageIcon("Main_Project/assets/img/searchIcon.png"); //image for search
+        
         private static JButton addButton = new JButton("Add New Product");
         private static JButton updateButton = new JButton("Update Product");
         private static JButton deleteButton = new JButton("Delete Product"); 
-        private static JLabel controlsTitle = new JLabel("Product Lookup");
-        private static JLabel productIDLabel = new JLabel("Product ID");
-        private static JTextField productIDField = new JTextField(20);
-        private static JLabel quantityLabel = new JLabel("Quantity");
-        private static JTextField quantityField = new JTextField(20);
-        private static JLabel wholeSaleLabel = new JLabel("Whole Sale Price");
-        private static JTextField wholeSaleField = new JTextField(20);
-        private static JLabel salePriceLabel = new JLabel("Sale Price");
-        private static JTextField salePriceField = new JTextField(20);
-        private static JLabel supplierIDLabel = new JLabel("Supplier ID");
-        private static JTextField supplierIDField = new JTextField(20);
-        private static JTable productsTable = new JTable();
-        private static JScrollPane tableScroll = new JScrollPane();
         private static JButton loadButton = new JButton("Load Inventory Data");
         private static JButton reloadButton = new JButton("Reload Inventory");
-        private static DbConnection linkDB;
-
-        ManageInventoryGUI(DbConnection linkDBIn){
+        private static JButton searchButton = new JButton(); //button with searchIcon.png
+        private static JButton backButton = new JButton("Back");
+        private static JButton logoutButton = new JButton("Logout");
+       
+        private static JTextField productIDField = new JTextField(20);
+        private static JTextField quantityField = new JTextField(20);
+        private static JTextField wholeSaleField = new JTextField(20);
+        private static JTextField salePriceField = new JTextField(20);
+        private static JTextField supplierIDField = new JTextField(20);
+        private static JTextField searchField = new JTextField(20);
+        
+         ManageInventoryGUI(DbConnection linkDBIn){
             
-            // setting the connection
+            // setting the connection with database
             linkDB = linkDBIn;
 
             // Importing and setting custom font Caveat for all text components 
@@ -67,6 +77,9 @@ public class ManageInventoryGUI implements ActionListener{
                 deleteButton.setFont(caveatFont);
                 loadButton.setFont(caveatFont);
                 reloadButton.setFont(caveatFont);
+                searchField.setFont(caveatFont);
+                backButton.setFont(caveatFont);
+                logoutButton.setFont(caveatFont);
                 footerLabel.setFont(caveatFont.deriveFont(16f));	    
             } catch (IOException | FontFormatException e) {
                 e.printStackTrace();
@@ -84,26 +97,36 @@ public class ManageInventoryGUI implements ActionListener{
             headerLabel.setVerticalTextPosition(JLabel.CENTER);
             headerLabel.setHorizontalTextPosition(JLabel.RIGHT);
 
+            //adding back button inside header panel
+            backButton.setBounds(0, 0, 70, 40);
+            backButton.setHorizontalAlignment(JLabel.LEFT);
+            backButton.setVerticalAlignment(JLabel.TOP);
+            backButton.addActionListener(this);
+           // headerPanel.add(backButton);
+           
+            //adding logout button inside header panel
+            logoutButton.setBounds(830, 0, 70, 40);
+            logoutButton.setHorizontalAlignment(JLabel.LEFT);
+            logoutButton.setVerticalAlignment(JLabel.TOP);
+            logoutButton.addActionListener(this);
+            //headerPanel.add(logoutButton);
+
             //add header label to header panel 
             headerPanel.add(headerLabel);
 
-            
-
-            /* ------- Body Content ------- */ 
+             /* ------- Body Content ------- */ 
             bodyPanel.setLayout(null);
             bodyPanel.setBackground(Color.LIGHT_GRAY);
             bodyPanel.add(controlsPanel);
         
-            
-
-            //CRUD controls Panel 
+             /* ------- CRUD Controls Panel------- */
             controlsPanel.setBounds(30, 30, 270, 500);
             controlsPanel.setBackground(Color.LIGHT_GRAY);
             controlsPanel.setLayout(new GridLayout(14, 1, 0, 5));
             controlsPanel.setBorder(BorderFactory.createEmptyBorder(0, 25, 0, 25));
             controlsTitle.setHorizontalAlignment(JLabel.CENTER);
             controlsPanel.add(controlsTitle); 
-            
+            //adding buttons/textfields to actual panel
             controlsPanel.add(addButton);
             addButton.addActionListener(this);
             controlsPanel.add(productIDLabel);
@@ -117,27 +140,39 @@ public class ManageInventoryGUI implements ActionListener{
             controlsPanel.add(supplierIDLabel);
             controlsPanel.add(supplierIDField);
             controlsPanel.add(updateButton);
-            // TODO: add event listenter to updateButton 
+            updateButton.addActionListener(this);
             controlsPanel.add(deleteButton);
-            // TODO: add event listenter to deleteButton 
-
-
-            //Table  Panel 
+            deleteButton.addActionListener(this);
+            
+            /* ------- Tabel Panel & Load/Reload Button ------- */
             tablePanel.setBounds(310, 0, 570, 520);
             tablePanel.setLayout(null);
             tablePanel.setBackground(Color.LIGHT_GRAY);
             bodyPanel.add(tablePanel);
 
-            loadButton.setBounds(200, 0, 200, 40);
+            loadButton.setBounds(370, 0, 200, 40);
             loadButton.addActionListener(this);
-            reloadButton.setBounds(200,0,200,40);
+            reloadButton.setBounds(370,0,200,40);
             reloadButton.addActionListener(this);
             tablePanel.add(loadButton);
             tableScroll.setBounds(0, 45, 570, 480);
             tablePanel.add(tableScroll);
             tableScroll.setViewportView(productsTable);
-            // TODO: add JTable to Table Panel and populate data from Product Inventory table in DB  
+            
+            /* ------- Search Textfield & Button ------- */
+            searchField.setBounds(0, 0, 200, 40);
+            searchField.setText("Search Product ID");
+            searchField.setHorizontalAlignment( JTextField.CENTER);
+            searchField.setForeground(Color.GRAY);
+            searchField.addFocusListener(this);
+            
+            tablePanel.add(searchField); 
         
+            searchButton.setBounds(200, 0, 40, 40);
+            searchButton.setIcon(searchImg);
+            searchButton.addActionListener(this);
+            
+            tablePanel.add(searchButton);
             
             /* ------- Footer Content ------- */
             //set defualts and text of footer panel
@@ -154,28 +189,19 @@ public class ManageInventoryGUI implements ActionListener{
             inventoryFrame.setSize(900,700);
             inventoryFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Exit out of the application
             inventoryFrame.setResizable(false); //this disable the resize features of the frame to the user
+            
 
             //add the header, body and footer panels to the frame 
             inventoryFrame.add(headerPanel, BorderLayout.NORTH);
             inventoryFrame.add(bodyPanel);
             inventoryFrame.add(footerPanel, BorderLayout.SOUTH);
         
-            //display all content to the GUI 
+            //displays all content to the GUI 
             inventoryFrame.setVisible(true);
 
-            }
+            }// end of ManageInventoryGUI(DbConnection linkDBIn)
 
-        /**********FOR TESTING PURPOSES************/
-    // public static void main(String[] args) {
-
-
-    //         new ManageInventoryGUI();
-
-    //     }
-
-
-    // event handler for butten click
-    @Override
+    @Override // event handler for button clicks
     public void actionPerformed(ActionEvent e) {
         //on click of Load button, dsiplay all data from product inventory table 
         if(e.getSource() == loadButton){
@@ -209,7 +235,38 @@ public class ManageInventoryGUI implements ActionListener{
         else if (e.getSource() == reloadButton){
             refreshProducts();
         }
+        else if(e.getSource() == logoutButton){
+            inventoryFrame.dispose();
+            new HomeGUI(); 
+
+        }
+        else if(e.getSource() == backButton){
+            inventoryFrame.dispose();
+            new EmployeeGUI(linkDB);
+            
+
+        }
     }
+
+    @Override //focusGained & focusLost Override's both belong to searchField & searchButton
+    public void focusGained(FocusEvent e){
+        if(e.getSource() == searchField && searchField.getText().equals("Search Product ID")){
+            searchField.setText("");
+            searchField.setForeground(Color.BLACK);
+            searchField.setHorizontalAlignment(JTextField.LEFT);
+            
+
+        }// end of if
+    }// end of focusGained
+    
+    @Override //focusGained & focusLost Override's both belong to searchField & searchButton
+    public void focusLost(FocusEvent e) {
+        if(searchField.getText().isEmpty()){
+            searchField.setText("Search Product ID");
+            searchField.setForeground(Color.GRAY);
+            searchField.setHorizontalAlignment(JTextField.CENTER);
+        }// end of if
+    }// end of focusLost
 
     //Method to refresh the productsTable JTable 
     public void refreshProducts(){
@@ -220,5 +277,7 @@ public class ManageInventoryGUI implements ActionListener{
 
 
     }
+
+    
        
 }
