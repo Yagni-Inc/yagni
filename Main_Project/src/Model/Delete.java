b928c42ca24082f75e5a29d9c69ecf5cf05708e3
Jesusxx-240 <98117780+Jesusxx-240@users.noreply.github.com>
2022-03-10 21:39:40 -0700

    Revert "Delete pop up to confirm delete"

--- Main_Project/src/Model/Delete.java
+++ Main_Project/src/Model/Delete.java
@@ -11,19 +11,23 @@ public class Delete {
         deleteID = deleteIDin;
 
     }
+
     public void delete(DbConnection linkDB) {
 
+        //Statement delete_statement = connection.createStatement();
         try {
             // Creates a statement object
-            Statement delete_statement = linkDB.getConnection().createStatement();                
+            Statement delete_statement = linkDB.getConnection().createStatement();
             // Calling the execute method to execute a DELETE statement with given ID
-            delete_statement.execute("DELETE FROM `yagni_inv_db`.`product` WHERE (`product_id` = '"  + deleteID + "');");
+            delete_statement.execute("DELETE FROM `yagni_inv_db`.`product` WHERE (`product_id` = '" + deleteID + "');");
             System.out.println("\nSuccessfully deleted the product from the inventory.");
             System.out.println("-----------------------------------------------------------------------------------");
         } catch (SQLException e) {
             System.out.println("Oops! An error has occured.");
             System.out.println(e);
         }
+
+
     
  }
 } 
