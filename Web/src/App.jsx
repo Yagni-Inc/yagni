81a823b593c1f5387a6bacb7b1c6b91dd1300e10
Chillyssa <38333607+chillyssa@users.noreply.github.com>
2022-04-03 19:50:38 -0600

    YAG-52-landing-page

--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/navBar/navBar";
 import Landing from "./pages/landing";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <Landing />
-    </div>
-  );
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
--- Web/src/index.jsx
+++ Web/src/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-import NavBar from "./components/navBar/navBar";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
