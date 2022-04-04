449cbca93d36ee1b606d19329cc300b93211ae2d
Chillyssa <38333607+chillyssa@users.noreply.github.com>
2022-04-04 15:49:20 -0600

    YAG-55-shop

---
+++ Web/src/pages/shop.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import ProductCard from "../components/productCard/productCard";
+
+class Shop extends Component {
+  state = {};
+  render() {
+    return <ProductCard />;
+  }
+}
+
+export default Shop;
