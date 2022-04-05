858a41e6b65a71888632db6944c102cf9fbd6c68
Seth Harling <sethharling@gmail.com>
2022-04-05 00:52:09 -0600

    started on Contact component as well as helper functions

---
+++ Web/src/components/contact/EmailValidator.js
@@ -0,0 +1,8 @@
+//This function will eventually validate that an email address is connected to a customer in our database.
+
+const emailValidator = email => {
+	console.log(email);
+	return true;
+};
+
+export default emailValidator;
