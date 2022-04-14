e35ae32b754dc9e0648931b9cccc217251b69b21
Chillyssa <38333607+chillyssa@users.noreply.github.com>
2022-04-14 14:18:54 -0600

    Integrating contact form to landing page

--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -3,7 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/navBar/NavBar';
 import Landing from './pages/Landing';
 import Shop from './pages/Shop';
-
+import Cart from './pages/Cart';
+import Orders from './pages/Orders';
+import ContactForm from './components/contact/Contact';
 import './App.css';
 
 class App extends Component {
@@ -14,8 +16,10 @@ class App extends Component {
 				<BrowserRouter>
 					<NavBar />
 					<Routes>
-						<Route path='/yagni' element={<Landing />} />
-						<Route path='/yagni/shop' element={<Shop />} />
+						<Route path='/' element={<Landing />} />
+						<Route path='/shop' element={<Shop />} />
+						<Route path='/cart' element={<Cart />} />
+						<Route path='/orders' element={<Orders />} />
 					</Routes>
 				</BrowserRouter>
 			</div>
