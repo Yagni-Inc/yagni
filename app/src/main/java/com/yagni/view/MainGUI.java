8dc67e1b147dc2fbb5a35ee4c8dc7b7cbe86b48d
Seth Harling <sethharling@gmail.com>
2022-04-24 12:14:46 -0600

    changed Main.java in view folder to MainGUI.java

--- app/src/main/java/com/yagni/view/MainGUI.java
+++ app/src/main/java/com/yagni/view/MainGUI.java
@@ -4,7 +4,7 @@ package com.yagni.view;
 Main Class is the entry point for GUI Java Application 
 */
 
-public class Main {
+public class MainGUI {
     public static void main(String[] args) {
         new HomeGUI();
     }
