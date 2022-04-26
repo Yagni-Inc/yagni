ac85bfd2cc6aad3fcc50f5828625372be2521c5c
Jordan_Snow <jordantreysnow@gmail.com>
2022-04-26 00:21:05 -0600

    Cleaned and organized with comments

--- app/src/main/java/com/yagni/controller/DbConnection.java
+++ app/src/main/java/com/yagni/controller/DbConnection.java
@@ -1,25 +1,36 @@
 package com.yagni.controller;
 
+//java sql library import 
+
 import java.sql.*;
 
+//Class decleration 
+
 public class DbConnection {
+
     // variables
+
     String userName, password;
     Connection linkDB;
 
-    public DbConnection(String userNameIn, String passwordIn){
+    // Method decleration
 
-        //variables
+    public DbConnection(String userNameIn, String passwordIn) {
+
+        // Method specific variables
+
         userName = userNameIn;
         password = passwordIn;
-        
+
         // setting up the connection object
+
         linkDB = connect(userName, password);
     }
 
-    public static Connection connect(String userName, String password){
-        
-        //Try block catches a class not found execption
+    public static Connection connect(String userName, String password) {
+
+        // Try block catches a class not found execption
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
@@ -27,31 +38,45 @@ public class DbConnection {
             System.out.println(e);
         }
 
-        //Try block catches SQLExecption
-        try{
-            //Making a connection to the database and setting it to a connection object
-            Connection linkDB = DriverManager.getConnection("jdbc:mysql://yag-01.cldkvvrrriir.us-west-1.rds.amazonaws.com:3306/yagni_inv_db",userName,password);
+        // Try block catches SQLExecption
 
-            //returning the connection object so it can be used through out the app
+        try {
+
+            // Making a connection to the database and setting it to a connection object
+
+            Connection linkDB = DriverManager.getConnection(
+                    "jdbc:mysql://yag-01.cldkvvrrriir.us-west-1.rds.amazonaws.com:3306/yagni_inv_db", userName,
+                    password);
+
+            // returning the connection object so it can be used through out the app
             return linkDB;
+
+            // Catch decleration for error handling of MySQL syntax issues
 
         } catch (SQLException e) {
             System.out.println(e);
-            
-            //if the connection does not work it returns null.
+
+            // Print statement for syntax error and null return
+
             System.out.println("ERROR: Connection to database failed.");
             return null;
         }
     }
-    
-    public Connection getConnection(){
+
+    // Method for connection establishing
+
+    public Connection getConnection() {
         return linkDB;
     }
-    
-    public void closeConnection(){
 
-        try{
-        linkDB.close();
+    // Method that closes the connection to the MySQL database
+
+    public void closeConnection() {
+
+        // Try and Catch for error handling for the closeConnection method
+
+        try {
+            linkDB.close();
         } catch (SQLException e) {
             System.out.println(e);
         }
