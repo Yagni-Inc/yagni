895ff760fd511437306b7da2f432b54a0239dfa2
Jordan_Snow <jordantreysnow@gmail.com>
2022-04-25 23:34:50 -0600

    Clean presentation and proper comments added

--- app/src/main/java/com/yagni/model/UserEncryption.java
+++ app/src/main/java/com/yagni/model/UserEncryption.java
@@ -1,16 +1,34 @@
- package com.yagni.model;
+package com.yagni.model;
+
+//Class name decleration
 
 public class UserEncryption {
 
+    // method within class delceration
+    // Method takes string value and returns cypered text
+
     public static String userEncryption(String value) {
-        // converts the string to a character array to alter each ascii value seperately
+
+        // converts the string to a character array to alter each ASCII value seperately
         char[] charVals = value.toCharArray();
+
+        // Variable declertion
         String hashedVal = "";
-        for (char c1: charVals) {
-            // adds a value of 3 to the current ascii or charater value being altered in the string array
+
+        // For loop to parse through string values and alter their ASCII value by set
+        // amount
+        for (char c1 : charVals) {
+
+            // adds a value of 3 to the current ascii or charatver value being altered in
+            // the string array
             c1 = (char) (c1 + 3);
+
+            // resets hashedVal to next element in the character array
             hashedVal += c1;
         }
+
+        // returns hashedVal to be used where method will be called
+
         return hashedVal;
     }
 }
