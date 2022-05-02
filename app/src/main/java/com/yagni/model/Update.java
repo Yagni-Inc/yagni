64f54336f04baf510d85182b2bfdfbbabf03f464
Jordan_Snow <jordantreysnow@gmail.com>
2022-05-01 19:29:53 -0600

    quick refactor of userEncryption and comment alteration on some files

--- app/src/main/java/com/yagni/model/Update.java
+++ app/src/main/java/com/yagni/model/Update.java
@@ -4,72 +4,76 @@ import java.sql.*;
 import com.yagni.controller.*;
 
 public class Update {
-	
-	private static String updateID, updateSupplierID, updateQuant, updatePrice, updateWholesale;
 
-	//Constructor
-    public Update(String productIdIn, String quantityIn, String wholeSaleIn, String salePriceIn, String supplierIdIn){
-        updateID = productIdIn;
+	// String Variables declaration
+	private static String updateID;
+	private static String updateSupplierID;
+	private static String updateQuant;
+	private static String updatePrice;
+	private static String updateWholesale;
+
+	// Constructor
+	public Update(String productIdIn, String quantityIn, String wholeSaleIn, String salePriceIn, String supplierIdIn) {
+
+		// String variable intitalization
+		updateID = productIdIn;
 		updateQuant = quantityIn;
 		updateWholesale = wholeSaleIn;
 		updatePrice = salePriceIn;
-        updateSupplierID = supplierIdIn;
-    }
+		updateSupplierID = supplierIdIn;
+	}
 
-	//UPDATE METHOD
-    public void update(DbConnection linkDB) {
+	// Update Method
+	public void update(DbConnection linkDB) {
 
-		//example sql statement to update a product:
-		//UPDATE `yagni_inv_db`.`product` SET `quanity` = '1614', `Whole_sale` = '135.92', `Sale_cost` = '207.56', `vendor_id` = 'WBWVYLRD' WHERE (`product_id` = '001LORWG0PC0');
-
-		//start the sql statement
+		// start the sql statement
 		String sql_statement = "UPDATE `yagni_inv_db`.`product` SET ";
-		
-		//initialize a counter to help with comma placement
+
+		// initialize a counter to help with comma placement
 		int count = 0;
 
-		if (updateQuant != null){ //if a new quantity is inputted
-			//add quantity statement to sql statement
+		if (updateQuant != null) { // if a new quantity is inputted
+			// add quantity statement to sql statement
 			sql_statement = sql_statement + "`quanity` = '" + updateQuant + "'";
-			//increase count by 1, so next item will have a comma before it
-			count ++;
+			// increase count by 1, so next item will have a comma before it
+			count++;
 		}
-		if (updateWholesale != null) { //if a new wholesale is inputted
+		if (updateWholesale != null) { // if a new wholesale is inputted
 			if (count > 0) {
-				//if there is a statement before, add a comma
+				// if there is a statement before, add a comma
 				sql_statement = sql_statement + ", ";
 			}
 			sql_statement = sql_statement + "`Whole_sale` = '" + updateWholesale + "'";
-			count ++;
+			count++;
 		}
 		if (updatePrice != null) {
 			if (count > 0) {
 				sql_statement = sql_statement + ", ";
 			}
 			sql_statement = sql_statement + "`Sale_cost` = '" + updatePrice + "'";
-			count ++;
+			count++;
 		}
 		if (updateSupplierID != null) {
 			if (count > 0) {
 				sql_statement = sql_statement + ", ";
 			}
 			sql_statement = sql_statement + "`vendor_id` = '" + updateSupplierID + "'";
-			count ++;
+			count++;
 		}
 
-		//finish sql statement off with final keywords/product ID
+		// finish sql statement off with final keywords/product ID
 		sql_statement = sql_statement + " WHERE (`product_id` = '" + updateID + "');";
 
 		try {
-            // Creates a statement object
-            Statement update_statement = linkDB.getConnection().createStatement();
-            // Calling the execute method to execute a DELETE statement with given ID
-            update_statement.execute(sql_statement);
-            System.out.println("\nSuccessfully updated product " + updateID + " in the inventory.");
-            System.out.println("-----------------------------------------------------------------------------------");
-        } catch (SQLException e) {
-            System.out.println("Oops! An error has occurred.");
-            System.out.println(e);
-        }
-    }
+			// Creates a statement object
+			Statement update_statement = linkDB.getConnection().createStatement();
+			// Calling the execute method to execute a DELETE statement with given ID
+			update_statement.execute(sql_statement);
+			System.out.println("\nSuccessfully updated product " + updateID + " in the inventory.");
+			System.out.println("-----------------------------------------------------------------------------------");
+		} catch (SQLException e) {
+			System.out.println("Oops! An error has occurred.");
+			System.out.println(e);
+		}
+	}
 }
