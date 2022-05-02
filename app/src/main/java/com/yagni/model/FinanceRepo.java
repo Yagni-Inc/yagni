721ef44d31724ebb96dcdc7b68bb4f09a1abae35
S4kRiPp4 <98367301+S4kRiPp4@users.noreply.github.com>
2022-05-01 20:32:55 -0600

    line wrapped FinanceRepo.java

--- app/src/main/java/com/yagni/model/FinanceRepo.java
+++ app/src/main/java/com/yagni/model/FinanceRepo.java
@@ -2,16 +2,15 @@ package com.yagni.model;
 
 import java.sql.*;
 import javax.swing.*;
-
 import com.yagni.controller.*;
 
 public class FinanceRepo {
 
+    // String variable declarations
     private static String newOrders;
     private static String oldOrders;
     private static String newDollar;
     private static String oldDollar;
-    
     
     public FinanceRepo(){
         
@@ -22,17 +21,22 @@ public class FinanceRepo {
             Statement state = linkDb.getConnection().createStatement();
 
             //querying data for newOrders "current week order count"
-            ResultSet result = state.executeQuery("SELECT Count(*) FROM yagni_inv_db.purchase_history where order_date >= DATE_ADD(curdate(),INTERVAL -1 WEEK) AND order_date <= curdate();");
+            ResultSet result = state.executeQuery("SELECT Count(*) FROM yagni_inv_db.purchase_history "
+                    + "where order_date >= DATE_ADD(curdate(),INTERVAL -1 WEEK) "
+                    + "AND order_date <= curdate();");
             result.next();
             newOrders = result.getString(1);
 
             //querying data for newDollar "current week dollar sum"
-            result = state.executeQuery("SELECT SUM(Sale_cost) FROM yagni_inv_db.purchase_history LEFT JOIN yagni_inv_db.product ON purchase_history.product_id = product.product_id where order_date >= DATE_ADD(curdate(),INTERVAL -1 WEEK) AND order_date <= curdate();");
+            result = state.executeQuery("SELECT SUM(Sale_cost) FROM yagni_inv_db.purchase_history "
+                    + "LEFT JOIN yagni_inv_db.product ON purchase_history.product_id = product.product_id "
+                    + "where order_date >= DATE_ADD(curdate(),INTERVAL -1 WEEK) AND order_date <= curdate();");
             result.next();
             newDollar = result.getString(1);
 
             //querying data for oldOrders "previous week order count"
-            result = state.executeQuery("SELECT COUNT(*) FROM yagni_inv_db.purchase_history where order_date >= DATE_ADD(curdate(),INTERVAL -2 WEEK) AND order_date <= DATE_ADD(curdate(),INTERVAL -1 WEEK);");
+            result = state.executeQuery("SELECT COUNT(*) FROM yagni_inv_db.purchase_history where order_date >= "
+                    + "DATE_ADD(curdate(),INTERVAL -2 WEEK) AND order_date <= DATE_ADD(curdate(),INTERVAL -1 WEEK);");
             result.next();
             oldOrders = result.getString(1);
 
@@ -45,7 +49,10 @@ public class FinanceRepo {
             diffOrders = newishOrders - oldishOrders;
             
             //querying data for oldDollar "previous week dollar sum"
-            result = state.executeQuery("SELECT SUM(Sale_cost) FROM yagni_inv_db.purchase_history LEFT JOIN yagni_inv_db.product ON purchase_history.product_id = product.product_id where order_date >= DATE_ADD(curdate(),INTERVAL -2 WEEK) AND order_date <= DATE_ADD(curdate(),INTERVAL -1 WEEK);");
+            result = state.executeQuery("SELECT SUM(Sale_cost) FROM yagni_inv_db.purchase_history "
+                    + "LEFT JOIN yagni_inv_db.product ON purchase_history.product_id = product.product_id "
+                    + "where order_date >= DATE_ADD(curdate(),INTERVAL -2 WEEK) "
+                    + "AND order_date <= DATE_ADD(curdate(),INTERVAL -1 WEEK);");
             result.next();
             oldDollar = result.getString(1);
 
@@ -60,14 +67,16 @@ public class FinanceRepo {
             //rounding diffDollar to hundredths decimal since we don't want to see fractions of a penny
             double roundDiff = Math.round(diffDollar*100.0)/100.0;
             
-
             //creating a pop-up that displays a message w/ variables created earlier
-            JOptionPane.showMessageDialog(null,"<html>New customer orders this week: " +newOrders+"<br>Dollar total of new orders this week: $" +newDollar+"<br>Customer orders difference from last week vs. this week: " +diffOrders+"<br>Dollar total difference of last week vs. this week: $" +roundDiff+"</html>", "Weekly Finance Report", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null,
+                    "<html>New customer orders this week: " + newOrders + "<br>Dollar total of new orders this week: $"
+                    + newDollar + "<br>Customer orders difference from last week vs. this week: " + diffOrders
+                    + "<br>Dollar total difference of last week vs. this week: $" + roundDiff + "</html>",
+              "Weekly Finance Report", JOptionPane.INFORMATION_MESSAGE);
 
         }catch (SQLException e) {
             System.out.println(e);
-            System.out.println("Oops an error occured!");
+            System.out.println("Oops an error occurred!");
         } 
     }
-    
 }
